acd8a5ec1af7c5908cf36bde03e674d218a2cbee
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-06-12 20:32:19 +0530

    add simple terraform project

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "ami_value" {
+  description = "value for the ami"
+  type = string
+}
+
+variable "instance_type_value" {
+  description = "value for instance type"
+}
+
+variable "subnet_id_value" {
+  description = "value for the subnet_id"
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami_value
+    instance_type = var.ami_value
+    subnet_id = var.subnet_id_value
+}
