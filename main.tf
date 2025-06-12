66ce20760d2d723ae7c362fefe4313161978c944
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-06-12 21:38:23 +0530

    sucessfully  created first ec2 instance through terraform

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-variable "ami_value" {
-  description = "value for the ami"
-  type = string
-}
 
-variable "instance_type_value" {
-  description = "value for instance type"
-}
-
-variable "subnet_id_value" {
-  description = "value for the subnet_id"
-}
 
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-north-1"
 }
 
 resource "aws_instance" "example" {
     ami = var.ami_value
-    instance_type = var.ami_value
+    instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
 }
