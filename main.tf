491d417a7a55ff8695f4e6e5dcbfb192f6ccf3db
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-06-15 20:31:02 +0530

    instance type change according to workspace

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = var.ami_value
-  instance_type_value = var.instance_type_value
+  instance_type_value = lookup(var.instance_type_value,terraform.workspace,"t2.micro")
 }
 
 /*module "aws_s3_bucket" {
--- variables.tf
+++ variables.tf
@@ -4,4 +4,11 @@ variable "ami_value" {
 
 variable "instance_type_value" {
   description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
 }
